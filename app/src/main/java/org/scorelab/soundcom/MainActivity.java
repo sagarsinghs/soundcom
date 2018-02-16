b3f0ffb25da3d28a5e1f1c84784757aa97c582fe
sagarsinghs <35603936+sagarsinghs@users.noreply.github.com>
2018-02-16 12:25:25 -0800

    Updated the MainActivity with the fontStyle textview and other features.

--- app/src/main/java/org/scorelab/soundcom/MainActivity.java
+++ app/src/main/java/org/scorelab/soundcom/MainActivity.java
@@ -1,27 +1,15 @@
 package org.scorelab.soundcom;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.os.Handler;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView startBtn;
@@ -29,19 +17,32 @@ public class MainActivity extends AppCompatActivity {
     private int seconds=0;
     private boolean startRun;
 
+    TextView imageViewScore2;
+    ImageButton imageButtonStart;
+
+    Button done;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //imageViewScore= (TextView) findViewById(R.id.imageViewScore2);
+        imageButtonStart =(ImageButton) findViewById(R.id.imageButtonStart) ;
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "RursusCompactMono.ttf");
+        ((TextView)findViewById(R.id.imageViewScore2)).setTypeface(font);
+
         getSupportActionBar().hide();
         if(savedInstanceState != null){
             seconds = savedInstanceState.getInt("seconds");
             startRun=savedInstanceState.getBoolean("startRun");
         }
 
-        startBtn = (ImageView)findViewById(R.id.button1);
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        done =(Button) findViewById(R.id.done);
+       // startBtn = (ImageView)findViewById(R.id.imageButtonStop1);
+        imageButtonStart.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -53,16 +54,18 @@ public class MainActivity extends AppCompatActivity {
                 startRun=true;
 
 
-              //  finish(); //because I don't want to close the UI after service started
+                finish(); //because I don't want to close the UI after service started
             }
         });
     }
-    public void onSaveInstanceState(Bundle saveInstanceState){
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        super.onSaveInstanceState(saveInstanceState);
         saveInstanceState.putInt("seconds", seconds);
         saveInstanceState.putBoolean("startRun", startRun);
     }
     private void Timer(){
-        final TextView timeView = (TextView)findViewById(R.id.textView4);
+     //   final TextView timeView = (TextView)findViewById(R.id.textView4);
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
@@ -73,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
                 String time = String.format("%d:%02d:%02d", hours, minutes, secs);
 
-                timeView.setText(time);
+                done.setText(time);
 
                 if(startRun){
                     seconds++;
